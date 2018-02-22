630b29309881af88bb58c59fc2ebd9b2ec8fce54
Nina Musillo <nmusillo520@gmail.com>
2018-02-21 19:18:15 -0500

    did some work with views and routing

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hallo World!'))
+
+app.get('/', function (req, res) { 
+	//res.send('Hullo World!')
+	res.render('index');
+})
+
+app.get('/About', function (req, res) {
+	res.render("about")
+})
+
+app.get('/Contact%20Us', function (req, res) {
+        res.render("contact-us")
+})
+
+app.get('/Home', function (req, res) {
+        res.render("index")
+})
+
+app.get('/Sign%20Up', function (req, res) {
+        res.render("signup")
+})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
